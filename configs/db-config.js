cd1467fbd9b8749b35f3ff7d8b69d0aed1b6ff9f
Ismail <ilm68265@gmail.com>
2025-04-27 12:24:41 +0500

    local config

--- configs/db-config.js
+++ configs/db-config.js
@@ -1,26 +1,3 @@
-// const { Sequelize } = require("sequelize");
-
-// const sequelize = new Sequelize(
-//   process.env.MYSQLDATABASE,
-//   process.env.MYSQLUSER,
-//   process.env.MYSQLPASSWORD,
-//   {
-//     host: process.env.MYSQLHOST,
-//     port: process.env.MYSQLPORT,
-//     dialect: "mysql",
-//     logging: false,
-//     dialectModule: require("mysql2"),
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000,
-//     },
-//   }
-// );
-
-// module.exports = sequelize;
-
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
@@ -30,22 +7,45 @@ const sequelize = new Sequelize(
   {
     host: process.env.MYSQLHOST,
     port: process.env.MYSQLPORT,
-    dialect: "mssql", // Change from "mysql" to "mssql"
+    dialect: "mysql",
     logging: false,
-    dialectModule: require("tedious"), // Use tedious instead of mysql2
+    dialectModule: require("mysql2"),
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000,
     },
-    dialectOptions: {
-      options: {
-        encrypt: true, // For Azure SQL Database connections
-        trustServerCertificate: false, // Change to true if you have certificate issues
-      },
-    },
   }
 );
 
 module.exports = sequelize;
+
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize(
+//   process.env.MYSQLDATABASE,
+//   process.env.MYSQLUSER,
+//   process.env.MYSQLPASSWORD,
+//   {
+//     host: process.env.MYSQLHOST,
+//     port: process.env.MYSQLPORT,
+//     dialect: "mssql", // Change from "mysql" to "mssql"
+//     logging: false,
+//     dialectModule: require("tedious"), // Use tedious instead of mysql2
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//     dialectOptions: {
+//       options: {
+//         encrypt: true, // For Azure SQL Database connections
+//         trustServerCertificate: true, // Change to true if you have certificate issues
+//       },
+//     },
+//   }
+// );
+
+// module.exports = sequelize;
